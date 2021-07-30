7b195748f3a504060a6a5a9d30311ff3199db812
DonaldoAr <jefe.ceo@gmail.com>
2021-07-29 20:19:12 -0500

    Add a fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,7 +3,21 @@
 import React from 'react';
 
 const PrimeraApp = ()=>{
-    return <h1> Hola mundo </h1>;
+
+    return (
+        // Envolverse en un  <div></div> para evitar errores
+        // When uses a <div></div> it happens that will have a several information
+        // we should uses a fragment instead
+        //<div>
+        //<Fragment >
+        // <></> this is a fragment
+        <> 
+            <h1> Hola mundo </h1>
+            <p>Mi primera apli</p>
+        </>
+        //</Fragment>
+        //</div>
+        );
 }
 
 export default PrimeraApp;
