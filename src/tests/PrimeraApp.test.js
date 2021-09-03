737c83743a63adb5212f95965d15ce9c4662d7f0
DonaldoAr <jefe.ceo@gmail.com>
2021-09-02 19:17:36 -0500

    Add a test with ENZYME

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
+//import { render } from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
 
 describe('Test about <PrimeraApp />', ()=>{
+    /*
     test('It should be give a message "Hola, Luis"', () => {
        const saludo = 'Hola Luis';
        // We need another const for render product
        const wrapper = render( <PrimeraApp saludo = {saludo} />);
         expect( wrapper.getByText(saludo) ).toBeInTheDocument();
-    }); 
+    });
+    */ 
+   test('It should show <PrimeraApp /> correctamente', () => {
+        const saludo = 'Hola Luis';
+        const wrapper = shallow( <PrimeraApp saludo = {saludo}/>);
+        expect( wrapper ).toMatchSnapshot();
+   })
+   
 });
